d0cecc197b48f687dfb32257354f541ee34b3a1e
manish257 <manishmahato257@gmail.com>
2022-06-07 13:03:22 +0500

    http commit

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,32 +1,31 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
 
 const courses = [
-    {id: 1, name: 'course1'},
-    {id: 1, name: 'course2'},
-    {id: 1, name: 'course3'},
+  { id: 1, name: "course1" },
+  { id: 1, name: "course2" },
+  { id: 1, name: "course3" },
 ];
 
-
-app.get('/', (req, res) => {
-    res.send('Hello Manish Mahato');
+app.get("/", (req, res) => {
+  res.send("Hello Manish Mahato");
 });
 // app.post()
 // app.put()
 // app.delete()
-app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with given ID was not found");
+  res.send(course);
 });
 
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query)
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
