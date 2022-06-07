323d5f40cb5e7e54db92018ca704c432446820ce
manish257 <manishmahato257@gmail.com>
2022-06-07 12:43:57 +0500

    commit with http get req

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,16 +1,32 @@
 const express = require('express'); 
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 1, name: 'course2'},
+    {id: 1, name: 'course3'},
+];
+
+
 app.get('/', (req, res) => {
-    res.send('Hello Manish');
+    res.send('Hello Manish Mahato');
 });
 // app.post()
 // app.put()
 // app.delete()
-
-app.listen(3000, () => console.log('Listening on port 3000...'));
-
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query)
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
