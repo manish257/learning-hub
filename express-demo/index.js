dc1867aa99fa3b88341da6c10b8ca45ed68845ed
manish257 <manishmahato257@gmail.com>
2022-06-07 12:05:47 +0500

    second commit with one get

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,16 @@
 const express = require('express'); 
 const app = express();
 
-app.get()
-app.post()
-app.put()
-app.delete()
+app.get('/', (req, res) => {
+    res.send('Hello Manish');
+});
+// app.post()
+// app.put()
+// app.delete()
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
