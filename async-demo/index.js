cc38ea9d64229dc62b938b142ab894f143da1855
manish257 <manishmahato257@gmail.com>
2022-06-09 16:59:58 +0500

    callbacks implemented

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,22 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from database...');
-}, 2000);
+getUser(1, (user) => {
+    console.log('User', user);
+    getRepositories(user.gitHubUsername, (repos) => {
+        console.log('Repos', repos);
+    });
+
+});
 console.log('After'); 
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from database...');
+        callback({ id: id, gitHubUsername: 'manish257' });
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
