f074acea2aaacf486f1fa879d6c47622650d8438
manish257 <manishmahato257@gmail.com>
2022-06-10 15:44:29 +0500

    with schema and collection

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB...'));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+const course = new Course({
+    name: 'Node.js Course',
+    author: 'Manish',
+    tags: ['node', 'backend'],
+    isPublished: true
+});
+
+
