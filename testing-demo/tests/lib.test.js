54a4a759802d425513e54cf15a100db4f102d7bd
manish257 <manishmahato257@gmail.com>
2022-06-16 16:59:53 +0500

    unit tests done separately

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,4 +1,17 @@
+const lib = require('../lib');
 
-test('Our first test', () => {
 
+test('absolute - should return a positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a 0 if input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(1);
 });
