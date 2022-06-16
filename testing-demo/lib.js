a8e86edafd9bc6d22eb38c929bcafd963c67eb0a
manish257 <manishmahato257@gmail.com>
2022-06-16 17:21:00 +0500

    some refactoring

--- testing-demo/lib.js
+++ testing-demo/lib.js
@@ -3,9 +3,9 @@
 
 // Testing numbers 
 module.exports.absolute = function(number) {
-  if (number > 0) return number; 
-  if (number < 0) return -number; 
-  return 0; 
+  return (number >= 0) ? number : -number; 
+  //if (number < 0) return -number; 
+  return -number; 
 }
 
 // // Testing strings 
