07085be021dd9f6551f8be1a6460f60bd8d6bdb7
manish257 <manishmahato257@gmail.com>
2022-06-29 12:44:33 +0500

    semantic ui

---
+++ blog/blog/src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class PostList extends Component {
+  render() {
+    return (
+      <div>Post List</div>
+    );
+  }
+}
+
+export default PostList;
