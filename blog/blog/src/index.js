5894413e322473e48b235d66654fac9ccf20b710
manish257 <manishmahato257@gmail.com>
2022-06-29 12:38:52 +0500

    redux starts

--- blog/blog/src/index.js
+++ blog/blog/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { configureStore } from 'redux';
-import './index.css';
+//import { configureStore } from '@reduxjs/toolkit';
+import { legacy_createStore as createStore } from '@reduxjs/toolkit';
 import App from './components/App';
 import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store = {configureStore(reducers)}>
+  <Provider store = {createStore(reducers)}>
     <App />
   </Provider>
 );
--- blog/blog/src/reducers/index.js
+++ blog/blog/src/reducers/index.js
@@ -1,3 +1,7 @@
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+export default combineReducers({
+    //dummy reducer
+    replaceMe: () => 'hi there'
+
+});
