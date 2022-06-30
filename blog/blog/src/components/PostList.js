b3c6709eb88ff866527cfd110ed333607ba82e39
manish257 <manishmahato257@gmail.com>
2022-06-30 16:20:56 +0500

    displaying users

--- blog/blog/src/components/PostList.js
+++ blog/blog/src/components/PostList.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 export class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-    componentDidMount() {
-        this.props.fetchPosts();
-    }
-
-
-    renderList() {
-      return this.props.posts.map(post => {
-        return (
-          <div className='item' key={post.id}>
-            <i className='large middle aligned icon user' />
-            <div className='content'>
-              <div className='description'>
-                <h2>
-                  {post.title}
-                </h2>
-                <p>
-                  {post.body}
-                </p>
-              </div>
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
             </div>
           </div>
-        );
-      });
-    }
+        </div>
+      );
+    });
+  }
 
   render() {
-    console.log(this.props.posts);
-    return (
-      <div>Post List</div>
-    );
+    //console.log(this.props.posts);
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
   return { posts: state.posts };
-}
+};
 
-export default connect(
-    mapStateToProps, 
-    { fetchPosts }
-)(PostList); 
+export default connect(mapStateToProps, { fetchPosts })(PostList);
