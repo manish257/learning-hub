c9f38a21e8e5ced06feb6905503201df1f3d1870
manish257 <manishmahato257@gmail.com>
2022-06-30 18:07:42 +0500

    one time memoization

--- blog/blog/src/actions/index.js
+++ blog/blog/src/actions/index.js
@@ -7,16 +7,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// export const fetchUser = (id) => async dispatch => {
-//   const reponse = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
 
-//   dispatch({ type: 'FETCH_USER', payload: reponse.data });
-// };
 
-export const fetchUser = function (id) {
-  return _.memoize(async function (dispatch) {
-    const reponse = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({ type: "FETCH_USER", payload: reponse.data });
-  });
-};
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const reponse = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: reponse.data });
+});
