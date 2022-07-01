4584cb7ce43f47b67db5c7c2bcf6e512f3971678
manish257 <manishmahato257@gmail.com>
2022-07-01 17:16:02 +0500

    links inside routers

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -5,13 +5,14 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 function App() {
   return (
-    <div>
-        <h1>HEADER</h1>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
