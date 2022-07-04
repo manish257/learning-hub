7e4b911ede0bb8f038db9603b4d88986dc9d9ae6
manish257 <manishmahato257@gmail.com>
2022-07-04 19:04:08 +0500

    some parts of consumer

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 export class Button extends Component {
-  static contextType = LanguageContext;
+//   static contextType = LanguageContext; // don use with consumer property
   render() {
     // console.log(this.context);
     const text = this.context === 'english' ? 'Submit' : 'Pesh Garnuhos';
