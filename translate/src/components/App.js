8afc9cce8bba217216e0d7e5cdc32fac7d8850e4
manish257 <manishmahato257@gmail.com>
2022-07-05 12:35:11 +0500

    making language selector

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
-
+import LanguageSelector from "./LanguageSelector";
 export class App extends Component {
   state = { language: "english" };
 
@@ -13,17 +13,8 @@ export class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="flag np"
-            onClick={() => this.onLanguageChange("nepali")}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+
         <LanguageContext.Provider value={this.state.language}>
           <ColorContext.Provider value="red">
             <UserCreate />
