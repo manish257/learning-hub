ab2d1e363087d131baebe8e83948136d594aa671
manish257 <manishmahato257@gmail.com>
2022-07-18 18:32:03 +0500

    used configureStore

--- react-tut/react-redux/src/App.js
+++ react-tut/react-redux/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import {actions} from './store/index';
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const increment = () => {
-    dispatch({ type: "INC" });
+    // dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
 
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    // dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
 
   const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 })
+    // dispatch({ type: "ADD", payload: 10 })
+    dispatch(actions.addBy(10));
   }
 
   return (
@@ -23,9 +27,9 @@ function App() {
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}> Add 10</button>
+      <button onClick={addBy}>Add 10</button>
     </div>
   );
-}
+};
 
 export default App;
