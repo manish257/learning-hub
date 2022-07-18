ae17ff4a89e5128f6bcdc97aca52a3a54500c93c
manish257 <manishmahato257@gmail.com>
2022-07-18 17:19:33 +0500

    counter app complete

--- react-tut/react-redux/src/store/index.js
+++ react-tut/react-redux/src/store/index.js
@@ -1,8 +1,23 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore } from "redux";
 
 const reducerFn = (state = { counter: 0 }, action) => {
-    return state;
-}
+  //synchronous function
+  //we should not mutate the original state
+
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+
+  if (action.type === 'ADD') {
+    return { counter: state.counter + action.payload };
+  }
+
+  return state;
+};
 
 const store = createStore(reducerFn);
 export default store;
