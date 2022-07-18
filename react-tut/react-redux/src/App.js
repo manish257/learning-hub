74bd6b7d664b1e59fcefa692b41a07cff35883c7
manish257 <manishmahato257@gmail.com>
2022-07-18 15:15:45 +0500

    making store

--- react-tut/react-redux/src/App.js
+++ react-tut/react-redux/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
   return (
     <div className="App">
       <h1>
         Counter App
       </h1>
+      <h2>{counter}</h2>
     </div>
   );
 }
